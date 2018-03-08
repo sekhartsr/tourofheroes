26ab2beedd39e2895cc93439eceaa4e1877109e0
Sekhar Tunuri <sekhar.tunuri@bestinet.com.my>
2018-03-08 18:27:32 +0800

    first commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Aanvith' },
+    { id: 12, name: 'Sachin' },
+    { id: 13, name: 'Dravid' },
+    { id: 14, name: 'Kumble' },
+    { id: 15, name: 'Srikanth' },
+    { id: 16, name: 'Ronaldo' },
+    { id: 17, name: 'Messi' },
+    { id: 18, name: 'Federer' },
+    { id: 19, name: 'Lokesh' },
+    { id: 20, name: 'Suresh' }
+  ];
